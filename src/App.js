86c7b4045b1d5253b87946e052f20fb3bd28a073
Oscar Jansson <oxarzik@hotmail.com>
2022-03-31 16:08:53 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Section from './components/Section';
+import HeroSection from './components/HeroSection';
+import Navbar from './components/Navbar';
 
-function App() {
+const App = () => {
+  const genreIncrement = 4
+  const [limit, setLimit] = useState(genreIncrement)
+  const [genres, setGenres] = useState(null)
+
+  const fetchData = async () => {
+    const response = await fetch("/.netlify/functions/getGenres", {
+      method: "POST",
+      body: limit
+    })
+    const responseBody = await response.json()
+    setGenres(responseBody.data.reference_list.values)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [, limit])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar />
+    <HeroSection />
+      {genres && (
+        <div className='container'>
+        {Object.values(genres).map((genre, index) => (<Section key={index} genre={genre.value}/>
+        ))}
+        </div>
+        )}
+      <div className='page-end' 
+      onMouseEnter={() => {
+        setLimit(limit + genreIncrement)
+      }}
+      />
+    </>
   );
 }
 
